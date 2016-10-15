f952467f6acace285bbe830373db27e1e9417e48
Jason Brown <jasonkpb@gmail.com>
2016-10-14 23:06:38 -0500

    Everything except 6...done in record time...lol

--- maze.js
+++ maze.js
@@ -1,19 +1,54 @@
+//Jason Brown 620080066
+
 window.onload = function(){ //load elements when the page/window loads
 
   //get all boundaries
 	var start = document.getElementById("start");
   var end = document.getElementById("end");
-	var boundary1 = document.getElementById("boundary1");
-	var boundary = document.querySelectorAll(".boundary");
+	var b1 = document.getElementById("boundary1");
+	var b = document.querySelectorAll(".boundary");//array of elements
   var hit = false;
 
-	function mouseOver(){ //handler for mouseOver boundary
-		boundary1.className = "boundary youlose"; //change the classes
-		for (var i = 0; i<boundary.length-1; i++)
-    {
-			boundary[i].className = "boundary youlose"; //attach this class name to the current class name
-		}
+  start.addEventListener("mouseover", beginMaze);//begin when over S
 
+	function mouseOver()
+  {
+    //handler for mouseOver boundary
+    hit = true; //boundary has been touched
+		b1.className = "youlose boundary"; //change the classes
+		for (var i = 0; i<b.length-1; i++)
+    {
+			b[i].className = "youlose boundary";
+		}
+	};
+
+  function beginMaze()
+  {
+		start.addEventListener("click", restart);
+		b1.addEventListener("mouseover", mouseOver);
+
+		for (var j = 0; j < b.length-1; j++){
+			b[j].addEventListener("mouseover", mouseOver);
+		}
+		end.addEventListener("mouseover", endMaze);//end the maze when mouse hits E
+	};
+
+  function restart()
+  {
+    //reloads by clicking s
+		location.reload();
+	};
+
+	function endMaze()
+  {
+			if (hit)//hit a wall?
+      {
+				document.getElementById("status").innerHTML = "You lose!";
+			}
+      else
+      {
+				document.getElementById("status").innerHTML = "You win!";
+			}
 	};
 
 };
